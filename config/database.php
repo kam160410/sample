371776d1624ec32d91aeab665d0855242f85bb42
huwenkang <kam160410@163.com>
2018-08-20 05:56:04 +0000

    fix

--- config/database.php
+++ config/database.php
@@ -73,7 +73,7 @@ return [
          'pgsql' => [
             'driver' => 'pgsql',
             'host' => $db_config['host'],
-            'port' => env('PG_DB_PORT', '5432'),
+            'port' => env('DB_PORT', '5432'),
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
